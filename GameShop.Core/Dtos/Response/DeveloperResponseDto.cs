77025e6cb8cda8854875e22ff30022f8f88299c8
m1toi <147705211+m1toi@users.noreply.github.com>
2024-04-17 16:48:52 +0300

    create dtos

---
+++ GameShop.Core/Dtos/Response/DeveloperResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GameShop.Core.Dtos.Response
+{
+    public class DeveloperResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<GameResponseDto> Games { get; set; }
+    }
+}
---
+++ GameShop.Core/Dtos/Response/GameResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GameShop.Core.Dtos.Response
+{
+    public class GameResponseDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
