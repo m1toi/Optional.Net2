90a9a2cdc1e8f808ebd44b6938f512a2ae025c1b
m1toi <147705211+m1toi@users.noreply.github.com>
2024-04-17 16:57:40 +0300

    create GameMappingExtensions.cs

---
+++ GameShop.Core/Mapping/GameMappingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using GameShop.Core.Dtos.Response;
+using GameShop.Database.Entities;
+
+namespace GameShop.Core.Mapping
+{
+    public static class GameMappingExtensions
+    {
+        public static GameResponseDto ToGameResponseDto(this Game game)
+        {
+            var result = new GameResponseDto
+            {
+                Id = game.Id,
+                Title = game.Title
+            };
+            return result;
+        }
+    }
+}
