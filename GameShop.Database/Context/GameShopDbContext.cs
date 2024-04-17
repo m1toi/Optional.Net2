b9e67be312f01100b7598487648aa29802dad552
m1toi <147705211+m1toi@users.noreply.github.com>
2024-04-17 14:58:12 +0300

    create GameShopDbContext.cs

---
+++ GameShop.Database/Context/GameShopDbContext.cs
@@ -0,0 +1,13 @@
+﻿using GameShop.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameShop.Database.Context
+{
+    public class GameShopDbContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+        public DbSet<GameDeveloper> Developers { get; set; }
+        public GameShopDbContext(DbContextOptions<GameShopDbContext> options) : base(options) { }
+
+    }
+}
