3f5b2232804643063c5a6913138eb1baf0393be7
m1toi <147705211+m1toi@users.noreply.github.com>
2024-04-17 17:02:17 +0300

    create DeveloperMappingExtensions.cs

--- GameShop.Core/Mapping/DeveloperMappingExtensions.cs
+++ GameShop.Core/Mapping/DeveloperMappingExtensions.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GameShop.Core.Dtos.Response;
+using GameShop.Database.Entities;
 
 namespace GameShop.Core.Mapping
 {
-    internal class DeveloperMappingExtensions
+    public static class DeveloperMappingExtensions
     {
+        public static DeveloperResponseDto ToDeveloperResponseDto(this GameDeveloper developer)
+        {
+            var result = new DeveloperResponseDto();
+            result.Id = developer.Id;
+            result.Name = developer.Name;
+
+            List<GameResponseDto> gamesDto = new List<GameResponseDto>();
+            foreach (var game in developer.Games)
+            {
+                gamesDto.Add(game.ToGameResponseDto());
+            }
+
+            result.Games = gamesDto;
+            return result;
+        }
     }
 }
