a4fcd80b7bdc8c4455e8e79e137744cd72c3a6c3
m1toi <147705211+m1toi@users.noreply.github.com>
2024-04-17 16:01:02 +0300

    create baserepository class

---
+++ GameShop.Database/Repositories/BaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using GameShop.Database.Context;
+
+namespace GameShop.Database.Repositories
+{
+    public class BaseRepository
+    {
+        protected GameShopDbContext _gameShopDbContext { get; set; }
+
+        public BaseRepository(GameShopDbContext libraryDbContext)
+        {
+            this._gameShopDbContext = libraryDbContext;
+        }
+
+        public void SaveChanges()
+        {
+            _gameShopDbContext.SaveChanges();
+        }
+
+    }
+}
