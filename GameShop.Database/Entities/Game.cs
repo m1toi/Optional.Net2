0756f444b717f001dac873fd4545397e92cb2e82
m1toi <147705211+m1toi@users.noreply.github.com>
2024-04-17 14:48:24 +0300

    create entities

---
+++ GameShop.Database/Entities/Game.cs
@@ -0,0 +1,9 @@
+﻿namespace GameShop.Database.Entities
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public virtual GameDeveloper Developer { get; set; }
+    }
+}
---
+++ GameShop.Database/Entities/GameDeveloper.cs
@@ -0,0 +1,9 @@
+﻿namespace GameShop.Database.Entities
+{
+    public class GameDeveloper
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual List<Game> Games { get;}
+    }
+}
