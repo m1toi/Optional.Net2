1df83eb55ee9460a456dd39e329db2f1a1dbed73
m1toi <147705211+m1toi@users.noreply.github.com>
2024-04-17 14:59:09 +0300

    update program.cs

--- GameShop.Api/Program.cs
+++ GameShop.Api/Program.cs
@@ -1,3 +1,6 @@
+using GameShop.Database.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.Development.json")
+    .Build();
+
+builder.Services.AddDbContext<GameShopDbContext>(
+    options => options.UseSqlServer(config.GetConnectionString("DatabaseConnection")));
 
 var app = builder.Build();
 
